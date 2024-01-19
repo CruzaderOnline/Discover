5a8ebe15220b013c5ac71e4543808e80f874bdbe
Cruz <douglas.mgs4@gmail.com>
2024-01-18 23:17:19 -0300

    Removed old folders, updated folder paths

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ const img = document.querySelector('#profile img')
 // substituir a imagem
 if(html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./projeto/assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos escuros e casaco preto, barba e fundo gradiente rosa e azul.")
     } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./projeto/assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
 
     }
 }
